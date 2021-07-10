2e2355b192c9f0e4732479487631398c19bf179e
Tuyen Dang <tuyendq@gmail.com>
2021-07-10 16:21:26 +0700

    Change src/App.js content

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Hello, 500 anh em!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
